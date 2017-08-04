2510b02b20ff46e7afe970d8e5153cc9fb1e9732
hossainbelal <30327614+hossainbelal@users.noreply.github.com>
2017-08-05 01:43:42 +0600

    class-12

---
+++ hs2-12/assets/js/main.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($){
+        
+        $(".team-carousel").owlCarousel({
+            items:3,
+            margin:30,
+            loop:true,
+            nav:true,
+            autoplay:true,
+            dots:true,
+            navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+          });
+        $(".product-carousel").owlCarousel({
+            items:4,
+            margin:30,
+            loop:true,
+            nav:true,
+            autoplay:true,
+            dots:true,
+            navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+          });
+        $(".homepage-slides").owlCarousel({
+            items:1,
+            loop:true,
+            nav:true,
+            autoplay:true,
+            dots:true,
+            navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+          });
+        $(".video-play-btn").magnificPopup({
+            type:'video',
+           });
+       });

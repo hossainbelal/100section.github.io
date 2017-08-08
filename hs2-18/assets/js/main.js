14cb28c162099f509dc0b20ece1bad306e61f15c
hossainbelal <30327614+hossainbelal@users.noreply.github.com>
2017-08-08 22:11:21 +0600

    class-18

---
+++ hs2-18/assets/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+    
+     	
+       });
+
+
+/* only for Masonry 
+
+   $(".portfilio-list").masonry({
+            horizontalOrder: true
+            });
+
+    *****/

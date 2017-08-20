74540d3ce862db449015a9ba92154fa0ccbe1bd9
hossainbelal <30327614+hossainbelal@users.noreply.github.com>
2017-08-20 22:23:27 +0600

    class-27

---
+++ hs2-27-lp1/assets/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+    	  $('.portfolio-item').owlCarousel({
+    	  	items:5,
+    	  	loop:true,
+    	  	nav:false,
+    	  	dots:true,
+    	  	autoplay:true,
+    	  });
+     	
+       });
+
+

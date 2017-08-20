f38272bf245f6ce2e90f2308dbb72900ab462907
hossainbelal <30327614+hossainbelal@users.noreply.github.com>
2017-08-21 00:52:45 +0600

    class-28 problem

---
+++ hs2-27-lp2(problem)/assets/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+    	  $(".portfolio-list").masonry({
+    	  			fitWidth: true, 
+    	  			horizontalOrder:false,
+    	  			percentPosition: true,
+    	  		
+    	  			
+    	  });
+     	
+       });
+
+

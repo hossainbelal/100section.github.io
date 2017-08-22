1f9912fc7597cd03fe27d05c7af96894ccc05b1f
hossainbelal <30327614+hossainbelal@users.noreply.github.com>
2017-08-22 23:30:48 +0600

    class-30

---
+++ hs2-30-lp4/assets/js/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($){
+    	  $(".portfolio-list").masonry({
+    	  			fitWidth: true, 
+    	  			horizontalOrder:false,
+    	  			percentPosition: true,  		
+    	  			
+    	  });
+
+    	  $(".single-portfolio-item").hover(function(){
+    	  	$(".single-portfolio-item .portfolio-hover h2").removeClass("animated slideInUp");
+    	  	$(this).find(".portfolio-hover h2").addClass("animated slideInUp");
+    	  });
+     	
+     		$(".menu-trigger").on('click', function(){
+     			$(".offcanvas-menu").addClass("active");
+     			$(".offcanvas-menu-overlay").addClass("active");
+     		});
+
+     		$(".menu-close i.fa, .offcanvas-menu-overlay").on('click', function(){
+     			$(".offcanvas-menu").removeClass("active");
+     			$(".offcanvas-menu-overlay").removeClass("active");
+     		});
+
+            $(".header-area").headroom();
+       });
+
+

bd9b90f61d573616666569f78b977fdab1e07412
hossainbelal <30327614+hossainbelal@users.noreply.github.com>
2017-09-22 11:17:48 +0600

    class-37

---
+++ hs2-37/assets/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+    	  $(".portfolio-list").masonry({
+    	  			//fitWidth: true, 
+    	  			//horizontalOrder:false,
+    	  			//percentPosition: true,  		
+    	  			
+    	  });
+
+     });
+
+
